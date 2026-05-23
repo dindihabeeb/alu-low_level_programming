70c9f44b3e355af5ed26169b4398f38cc252a336
Habeeb Dindi <habeebullahdindi@gmail.com>
2026-05-23 14:46:12 +0200

    feat: submit hello_world project

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	write(2, "\n", 1);
+	return (1);
+}
